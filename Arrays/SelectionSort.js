10e4762da7324cbb2c349ce4a84354a4acce7c27
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2026-02-20 00:35:43 +0530

    minor refactoring

--- Arrays/SelectionSort.js
+++ Arrays/SelectionSort.js
@@ -1,8 +1,8 @@
 const selectionSort = (arr) => {
   for (let i = 0; i < arr.length - 1; i++) {
-    let midIndx = i;
+    let minIndx = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[midIndx]) {
+      if (arr[j] < arr[minIndx]) {
         midIndx = j;
       }
     }
