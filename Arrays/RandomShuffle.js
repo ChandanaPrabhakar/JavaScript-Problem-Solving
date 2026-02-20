7b62614fc02c733132107088163b9f1cb3a296d2
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2026-02-20 14:38:27 +0530

    Shuffle an array randomly.

---
+++ Arrays/RandomShuffle.js
@@ -0,0 +1,9 @@
+const randomShuffle = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
+
+console.log(randomShuffle([1, 2, 3, 4, 5]));
