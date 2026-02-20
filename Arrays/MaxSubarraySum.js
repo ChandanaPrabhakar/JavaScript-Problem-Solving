35afca533afd53024f88bf359c656ab639339bed
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2026-02-20 19:52:56 +0530

    Find maximum subarray sum (Kadane basic).

---
+++ Arrays/MaxSubarraySum.js
@@ -0,0 +1,11 @@
+const maxSubArraySum = (arr) => {
+  let maxSum = arr[0];
+  let currentSum = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    currentSum = Math.max(arr[i], currentSum + arr[i]);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+  return maxSum;
+};
+
+console.log(maxSubArraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
